3a5d36a122d4c815536feeaf671dc387fa27f773
peter chen <3033844847@qq.com>
2025-12-16 16:55:34 +0800

    Site updated: 2025-12-16 16:55:32

---
+++ anzhiyu/random.js
@@ -0,0 +1,3 @@
+var posts=["year/11/08/11月的碎碎念/","year/11/03/ROS-raspi/","year/11/08/关于焦虑/","year/11/13/从零开始一个机械臂/","year/11/03/ROS基础/","year/11/08/关于电赛/"];function toRandomPost(){
+    pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
+  };
